79601721161fc2806c6d8848f188ed9a3e72d35a
burak <burakoncuy@gmail.com>
2025-02-24 16:09:34 -0500

    changes can be seen in the nav bar when you add item to fav or cart now

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,40 +4,44 @@ import "./Navigation.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getFavorites } from "../../redux/favorite";
-
+import { fetchCart } from "../../redux/cart";
 
 function Navigation() {
+  const dispatch = useDispatch();
+  const favorites = useSelector(state => state.favorites.favorites);
+  const cartItems = useSelector(state => state.cart.cartItems);
+  const user = useSelector(state => state.session.user || {});
 
-const dispatch = useDispatch()
-const fav = useSelector(state => Object.values(state.favorites))
-const user = useSelector(state => state.session.user || {})
+  useEffect(() => { 
+    if (user.id) {
+      dispatch(getFavorites());
+      dispatch(fetchCart());
+    }
+  }, [dispatch, user]);
 
-
-
-useEffect(() => { 
-  dispatch(getFavorites())
-},[dispatch])
-
-
-
-
-
-return (
-  <nav className="nav-bar">
-  <div className="main-div">
-    <NavLink to="/items">NowJersey
-    </NavLink>
-       <div className="second-sub-main">
-        {user.id && <NavLink to='/orders'>My Orders</NavLink>}
-        {user.id && <NavLink to='/items/current'>My Items</NavLink>}
-        {user.id && <NavLink to='/reviews/current'>My reviews</NavLink>}
-        {user.id && <NavLink to='/items/favorites'>My favorites</NavLink>}
-        {user.id && <NavLink to='/cart'> Cart</NavLink>}
-        <ProfileButton />
-    </div>
-  </div>
-  </nav>
-);
+  return (
+    <nav className="nav-bar">
+      <div className="main-div">
+        <NavLink to="/items">NowJersey</NavLink>
+        <div className="second-sub-main">
+          {user.id && <NavLink to='/orders'>My Orders</NavLink>}
+          {user.id && <NavLink to='/items/current'>My Items</NavLink>}
+          {user.id && <NavLink to='/reviews/current'>My Reviews</NavLink>}
+          {user.id && (
+            <NavLink to='/items/favorites' className={favorites.length > 0 ? 'highlight' : ''}>
+              My Favorites <span className="count-badge">{favorites.length}</span>
+            </NavLink>
+          )}
+          {user.id && (
+            <NavLink to='/cart' className={cartItems.length > 0 ? 'highlight' : ''}>
+              Cart <span className="count-badge">{cartItems.length}</span>
+            </NavLink>
+          )}
+          <ProfileButton />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
